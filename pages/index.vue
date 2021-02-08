3b00de666c886ddb34b00db9185b385facc42012
Shintaro Tamefusa <tameshin@gmail.com>
2021-02-09 01:01:58 +0900

    PythonのAPIを呼び出すように修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <section>
     <b-button @click="clickMe('a')">0.1</b-button>
-    <b-button @click="clickMe('b')">0.3</b-button>
-    <b-button @click="clickMe('c')">0.5</b-button>
-    <b-button @click="clickMe('d')">1.0</b-button>
+    <b-button @click="clickMe('0')">0.3</b-button>
+    <b-button @click="clickMe('1')">0.5</b-button>
+    <b-button @click="clickMe('2')">1.0</b-button>
   </section>
 </template>
 
@@ -11,10 +11,6 @@
 export default{
   methods: {
     async clickMe(value) {
-      this.$axios.$get('/api', {
-        params: {value: value}
-      })
-      /*
       this.$buefy.notification.open('Recognized- started.' + value)
       const SpeechRecognition =
         window.webkitSpeechRecognition || window.SpeechRecognition
@@ -23,6 +19,14 @@ export default{
       recognition.onresult = (e) => {
         const input = e.results[0][0].transcript
         this.$buefy.notification.open(input)
+
+        if (input == "上") {
+          this.$axios.$get('http://localhost:8080/api/1')
+        } else if (input == "右") {
+          this.$axios.$get('http://localhost:8080/api/2')
+        } else if (input == "下") {
+          this.$axios.$get('http://localhost:8080/api/0')
+        }
       }
 
       recognition.onend = (e) => {
@@ -30,7 +34,6 @@ export default{
       }
 
       recognition.start()
-      */
     },
   },
 }
