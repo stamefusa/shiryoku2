45bda4776140fad561d147334ee68cc8fea4add1
Shintaro Tamefusa <tameshin@gmail.com>
2021-02-09 00:59:44 +0900

    サーボを動かす処理をArduinoに追加

--- shiryoku2_ardiuno/shiryoku2_ardiuno.ino
+++ shiryoku2_ardiuno/shiryoku2_ardiuno.ino
@@ -1,9 +1,16 @@
+#include <Servo.h>
+
+Servo s1;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.setTimeout(10); // SerialでのString受信のタイムアウト設定（ms）
 
   pinMode(13, OUTPUT);
+
+  s1.attach(6);
+  s1.write(90);
 }
 
 void loop() {
@@ -18,6 +25,17 @@ void loop() {
       digitalWrite(13, HIGH);
     } else {
       digitalWrite(13, LOW);
+    }
+
+    if (key == "0") {
+      Serial.println("servo 0");
+      s1.write(5);
+    } else if (key == "1") {
+      Serial.println("servo 180");
+      s1.write(180);
+    } else if (key == "2") {
+      Serial.println("servo 90");
+      s1.write(90);
     }
   }
 
