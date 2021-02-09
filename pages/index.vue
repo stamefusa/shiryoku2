75f171dcb4266760f53e0a67b9e131a6b47942c9
Shintaro Tamefusa <tameshin@gmail.com>
2021-02-10 00:45:25 +0900

    Arduinoに合わせて修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <section>
     <b-button @click="clickMe('a')">0.1</b-button>
-    <b-button @click="clickMe('0')">0.3</b-button>
-    <b-button @click="clickMe('1')">0.5</b-button>
-    <b-button @click="clickMe('2')">1.0</b-button>
+    <b-button @click="clickMe('b')">0.5</b-button>
+    <b-button @click="clickMe('c')">1.0</b-button>
+    <b-button @click="clickMe('d')">2.0</b-button>
   </section>
 </template>
 
@@ -12,6 +12,10 @@ export default{
   methods: {
     async clickMe(value) {
       this.$buefy.notification.open('Recognized- started.' + value)
+      if (['a', 'b', 'c', 'd'].indexOf(value) != -1) {
+        this.$axios.$get('http://localhost:8080/api/'+value);
+      }
+
       const SpeechRecognition =
         window.webkitSpeechRecognition || window.SpeechRecognition
       const recognition = new SpeechRecognition()
@@ -21,11 +25,15 @@ export default{
         this.$buefy.notification.open(input)
 
         if (input == "上") {
+          this.$axios.$get('http://localhost:8080/api/0')
+        } else if (input == "右上") {
           this.$axios.$get('http://localhost:8080/api/1')
         } else if (input == "右") {
           this.$axios.$get('http://localhost:8080/api/2')
+        } else if (input == "右下") {
+          this.$axios.$get('http://localhost:8080/api/3')
         } else if (input == "下") {
-          this.$axios.$get('http://localhost:8080/api/0')
+          this.$axios.$get('http://localhost:8080/api/4')
         }
       }
 
